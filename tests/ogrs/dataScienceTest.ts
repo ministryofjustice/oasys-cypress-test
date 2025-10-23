c39c4940a3cbe03e4c462c23982046858d5eba0d
mhiorns-moj <martin.hiorns@justice.gov.uk>
2025-10-23 11:46:34 +0100

    Restructured DS test

--- tests/ogrs/dataScienceTest.ts
+++ tests/ogrs/dataScienceTest.ts
@@ -3,64 +3,27 @@ import { runTest } from './testLib'
 
 describe('OGRS calculator test - data science test cases', () => {
 
-    it('Data science test cases - static N, part 1', () => {
-
-        const csvTestParams: OgrsTestParameters = {
-            testType: 'csv',
-            csvDetails: {
-                dataFile: 'dsTestCases',
-            },
-            staticFlag: 'N',
-            reportMode: 'minimal',
-        }
-
-        csvTestParams.csvDetails.end = 4999
-        runTest(csvTestParams, 'dsOutputN1', true, true)
-    })
-
-    it('Data science test cases - static N, part 2', () => {
-
-        const csvTestParams: OgrsTestParameters = {
-            testType: 'csv',
-            csvDetails: {
-                dataFile: 'dsTestCases',
-            },
-            staticFlag: 'N',
-            reportMode: 'minimal',
-        }
-
-        csvTestParams.csvDetails.start = 5000
-        runTest(csvTestParams, 'dsOutputN2', true, true)
-    })
-
-    it('Data science test cases - static Y, part 1', () => {
-
-        const csvTestParams: OgrsTestParameters = {
-            testType: 'csv',
-            csvDetails: {
-                dataFile: 'dsTestCases',
-            },
-            staticFlag: 'Y',
-            reportMode: 'minimal',
-        }
-
-        csvTestParams.csvDetails.end = 4999
-        runTest(csvTestParams, 'dsOutputY1', true, true)
-    })
-
-    it('Data science test cases - static Y, part 2', () => {
-
-        const csvTestParams: OgrsTestParameters = {
-            testType: 'csv',
-            csvDetails: {
-                dataFile: 'dsTestCases',
-            },
-            staticFlag: 'Y',
-            reportMode: 'minimal',
-        }
-
-        csvTestParams.csvDetails.start = 5000
-        runTest(csvTestParams, 'dsOutputY2', true, true)
-    })
-
+    test(1, 'N')
+    test(2, 'N')
+    test(1, 'Y')
+    test(2, 'Y')
 })
+
+function test(part: number, staticFlag: 'Y' | 'N') {
+
+    it(`Data science test cases - static ${staticFlag}, part ${part}`, () => {
+
+        const csvTestParams: OgrsTestParameters = {
+            testType: 'csv',
+            csvDetails: {
+                dataFile: 'dsTestCases',
+                start: part == 1 ? 0 : 5000,
+                end: part == 1 ? 4999 : 9999,
+            },
+            staticFlag: staticFlag,
+            reportMode: 'minimal',
+        }
+
+        runTest(csvTestParams, `dsOutput${staticFlag}${part}`, true, true)
+    })
+}
