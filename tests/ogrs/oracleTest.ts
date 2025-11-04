7fa5fdcfe4160c8fa44a0621f478f1ec0515d954
mhiorns-moj <martin.hiorns@justice.gov.uk>
2025-11-04 09:31:45 +0000

    Updated test case selection for database tests

--- tests/ogrs/oracleTest.ts
+++ tests/ogrs/oracleTest.ts
@@ -42,18 +42,18 @@ function runTests(staticFlag: 'Y' | 'N') {
         runTest(dbTestParams, '', false, false)
     })
 
-    it(`Layer 3 v2 complete`, () => {
-
-        dbTestParams.dbDetails.type = 'assessment'
-        dbTestParams.dbDetails.whereClause = `deleted_date is null and ref_ass_version_code = 'LAYER3' and version_number = 2 and assessment_status_elm = 'COMPLETE'`
-        runTest(dbTestParams, '', false, false)
-    })
-
     it(`Layer 1 v2 complete`, () => {
 
         dbTestParams.dbDetails.type = 'assessment'
         dbTestParams.dbDetails.whereClause = `deleted_date is null and ref_ass_version_code = 'LAYER1' and version_number = 2 and assessment_status_elm = 'COMPLETE'`,
             runTest(dbTestParams, '', false, false)
+    })
+
+    it(`Layer 1 v2 not complete (any other status)`, () => {
+
+        dbTestParams.dbDetails.type = 'assessment'
+        dbTestParams.dbDetails.whereClause = `deleted_date is null and ref_ass_version_code = 'LAYER1' and version_number = 2 and assessment_status_elm <> 'COMPLETE'`
+        runTest(dbTestParams, '', false, false)
     })
 
     it(`Standalone RSR complete`, () => {
