b0f501ae02ea7021b03e4f0c0b80713f62b6a444
mhiorns-moj <martin.hiorns@justice.gov.uk>
2025-11-06 10:59:50 +0000

    Changes to run in MoJ environments

--- tests/regression/restApi/testAllApisMultipleOffenders.cy.ts
+++ tests/regression/restApi/testAllApisMultipleOffenders.cy.ts
@@ -1,5 +1,5 @@
 import * as oasys from 'oasys'
-import { restApiUrls } from 'environments'
+import { testEnvironment } from '../../../localSettings'
 
 /**
  * Tests all endpoints against a randomish set of 400 offenders based on specified date conditions covering 2015 to yesterday.
@@ -10,6 +10,7 @@ describe('RestAPI regression tests', () => {
     const startTime = Cypress.dayjs().format('YYYY-MM-DD HH:mm:ss')
 
     let stats: EndpointStat[] = []
+    let offendersTested = 0
 
     // Number of offenders for each date range
     const offenderCountEarly = 10  // Used for pre-2023, not many offenders available
@@ -17,15 +18,16 @@ describe('RestAPI regression tests', () => {
 
     // Define date parameters for sets of offender data
     const dateConditions = [
-        // { date: `2015-${randomMonth()}-${randomDay()}`, count: offenderCountEarly },
-        // { date: `2016-${randomMonth()}-${randomDay()}`, count: offenderCountEarly },
-        // { date: `2017-${randomMonth()}-${randomDay()}`, count: offenderCountEarly },
-        // { date: `2018-${randomMonth()}-${randomDay()}`, count: offenderCountEarly },
-        // { date: `2019-${randomMonth()}-${randomDay()}`, count: offenderCountEarly },
-        // { date: `2020-${randomMonth()}-${randomDay()}`, count: offenderCountEarly },
-        // { date: `2021-${randomMonth()}-${randomDay()}`, count: offenderCountEarly },
+        { date: `2015-${randomMonth()}-${randomDay()}`, count: offenderCountEarly },
+        { date: `2016-${randomMonth()}-${randomDay()}`, count: offenderCountEarly },
+        { date: `2017-${randomMonth()}-${randomDay()}`, count: offenderCountEarly },
+        { date: `2018-${randomMonth()}-${randomDay()}`, count: offenderCountEarly },
+        { date: `2019-${randomMonth()}-${randomDay()}`, count: offenderCountEarly },
+        { date: `2020-${randomMonth()}-${randomDay()}`, count: offenderCountEarly },
+        { date: `2021-${randomMonth()}-${randomDay()}`, count: offenderCountEarly },
         { date: `2022-${randomMonth()}-${randomDay()}`, count: offenderCountEarly },
         { date: `2023-${randomMonth()}-${randomDay()}`, count: offenderCount },
+        { date: `2024-${randomMonth()}-${randomDay()}`, count: offenderCount },
         { date: 'today', count: offenderCount },
     ]
 
@@ -55,32 +57,33 @@ describe('RestAPI regression tests', () => {
         })
     }
 
+    // Extra cases specific to Capita environments
     const extraTestCases = [
-        // ['AZ97320', null],
-        // ['A1111QC', null],
-        // ['DS12345', null],       // Assessment selection criteria
-        // ['a454545', null],       // Assessment selection criteria       
-        // ['ZYTGLMN', null],       // RMP timeline issue
-        // ['WS54109', null],      // Empty offence block
-        // ['290508', null],        // Number with leading zero
-        // ['D13079A', 'A2222BW'],
-        // ['CRN11AG', 'A1111AG'],
-        // ['CRN4094', 'C4094RN'],
-        // ['UGVAAAA', null],
-        // ['TRA001', null],
-        // ['ZFJWVOH', null],
-        // ['ZAAEUDK', null],
-        // ['ZPDMMDO', null],
-        // ['ZWXZHPN', null],
-        // ['ZYVSDTV', null],  // fullyPopulatedLayer1 (in system test on 29/11)
-        // ['ZFCJDLP', null],  // fullyPopulatedLayer3 (in system test on 29/11)
-        // ['ZCLUWYS', null],  // fullyPopulatedLayer3MaxStrings (in system test on 29/11)
-        // ['ZXRWTTK', 'Z6331QT'],  // fullyPopulatedLayer3MaxStringsPrison (in system test on 29/11)
-        // ['ZENIEZF', 'Z7251BN'],  // fullyPopulatedLayer3Prison (in system test on 29/11)
-        // ['ZJVGRBQ', null],  // fullyPopulatedLayer1v2 (in system test on 1/12)
-        // ['A1111QE', null],     // lots of assessments with lots of data
-        // ['CRN8826', null],      // offender has a LAYER2 assessment
-        // ['PS501143', null],     // objectiveDesc example
+        ['AZ97320', null],
+        ['A1111QC', null],
+        ['DS12345', null],       // Assessment selection criteria
+        ['a454545', null],       // Assessment selection criteria       
+        ['ZYTGLMN', null],       // RMP timeline issue
+        ['WS54109', null],      // Empty offence block
+        ['290508', null],        // Number with leading zero
+        ['D13079A', 'A2222BW'],
+        ['CRN11AG', 'A1111AG'],
+        ['CRN4094', 'C4094RN'],
+        ['UGVAAAA', null],
+        ['TRA001', null],
+        ['ZFJWVOH', null],
+        ['ZAAEUDK', null],
+        ['ZPDMMDO', null],
+        ['ZWXZHPN', null],
+        ['ZYVSDTV', null],  // fullyPopulatedLayer1 (in system test on 29/11)
+        ['ZFCJDLP', null],  // fullyPopulatedLayer3 (in system test on 29/11)
+        ['ZCLUWYS', null],  // fullyPopulatedLayer3MaxStrings (in system test on 29/11)
+        ['ZXRWTTK', 'Z6331QT'],  // fullyPopulatedLayer3MaxStringsPrison (in system test on 29/11)
+        ['ZENIEZF', 'Z7251BN'],  // fullyPopulatedLayer3Prison (in system test on 29/11)
+        ['ZJVGRBQ', null],  // fullyPopulatedLayer1v2 (in system test on 1/12)
+        ['A1111QE', null],     // lots of assessments with lots of data
+        ['CRN8826', null],      // offender has a LAYER2 assessment
+        ['PS501143', null],     // objectiveDesc example
     ]
 
     /* Lowercase examples - need fix for NOD-872 before adding these
@@ -89,7 +92,9 @@ describe('RestAPI regression tests', () => {
     */
 
     it(`All endpoint regression tests - extra test for specific cases`, () => {
-        runTest(extraTestCases)
+        if (testEnvironment.name.includes('system') || testEnvironment.name.includes('informal')) {
+            runTest(extraTestCases)
+        }
     })
 
     function runTest(offenders: string[][]) {
@@ -99,6 +104,7 @@ describe('RestAPI regression tests', () => {
 
         offenders.forEach((offender) => {
             cy.task('consoleLog', `Offender ${count++}: ${offender[0]} / ${offender[1]}`)
+            offendersTested++
 
             if (offender[0] != null) {  // call with probation CRN
                 oasys.Api.testOneOffender(offender[0], 'prob', 'probationFailedAlias', false, stats)
@@ -145,7 +151,7 @@ describe('RestAPI regression tests', () => {
 
         let elapsedTimeS = Math.round(Cypress.dayjs().diff(startTime) / 1000)
         cy.groupedLogStart('Totals')
-        cy.groupedLog(`Offenders: ${offenderCount * 2 + offenderCountEarly * 8 + extraTestCases.length}`)
+        cy.groupedLog(`Offenders: ${offendersTested}`)
         cy.groupedLog(`API calls: ${totalApiCount}`)
         cy.groupedLog(`Database query time: ${Math.round(totalDbTimeMs / 1000)}s`)
         cy.groupedLog(`API response time: ${Math.round(totalApiTimeMs / 1000)}s`)
