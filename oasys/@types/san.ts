13791987135ff63839ac08c4cca92fd570fc5d22
mhiorns-moj <martin.hiorns@justice.gov.uk>
2025-11-25 08:59:08 +0000

    SAN fixes and drugs

--- oasys/@types/san.ts
+++ oasys/@types/san.ts
@@ -17,7 +17,7 @@ declare type SanScenario = {
 declare type SanPopulation = { section: SanSection, steps: SanStep[] }[]
 
 declare type SanIds = { [keys: string]: SanId }
-declare type SanId = { type: 'radio' | 'checkbox' | 'textbox' | 'action' | 'date' | 'combo' | 'button' | 'select', id: string, options?: string[] }
+declare type SanId = { type: 'radio' | 'checkbox' | 'textbox' | 'action' | 'date' | 'combo' | 'button' | 'select' | 'conditionalCheckbox', id: string, options?: string[] }
 
 declare type SanSection = 'Accommodation' | 'Employment and education' | 'Finances' | 'Drug use' | 'Alcohol use' | 'Health and wellbeing'
     | 'Personal relationships and community' | 'Thinking, behaviours and attitudes' | 'Offence analysis' | 'Sentence plan'
