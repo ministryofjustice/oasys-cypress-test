0915624d893bc4d710401720e0d8829e06b016fa
mhiorns-moj <martin.hiorns@justice.gov.uk>
2026-01-14 10:36:34 +0000

    Added L1V1

--- tests/ogrs/oracleTest.ts
+++ tests/ogrs/oracleTest.ts
@@ -46,6 +46,20 @@ describe('OGRS calculator test', () => {
         runTest(dbTestParams, '', false, false)
     })
 
+    it(`Layer 1 v1 complete`, () => {
+
+        dbTestParams.dbDetails.type = 'assessment'
+        dbTestParams.dbDetails.whereClause = `deleted_date is null and ref_ass_version_code = 'LAYER1' and version_number = 1 and assessment_status_elm = 'COMPLETE'`,
+            runTest(dbTestParams, '', false, false)
+    })
+
+    it(`Layer 1 v1 not complete (any other status)`, () => {
+
+        dbTestParams.dbDetails.type = 'assessment'
+        dbTestParams.dbDetails.whereClause = `deleted_date is null and ref_ass_version_code = 'LAYER1' and version_number = 1 and assessment_status_elm <> 'COMPLETE'`
+        runTest(dbTestParams, '', false, false)
+    })
+
     it(`Standalone RSR complete`, () => {
 
         dbTestParams.dbDetails.type = 'rsr'
