bdc805f7d3e71c7392f07bbf45d47c96ec37ce50
mhiorns-moj <martin.hiorns@justice.gov.uk>
2026-02-04 15:53:08 +0000

    OGRS regression test (WIP)

--- tests/regression/nods/NOD-1148/testRef3/testRef3.cy.ts
+++ tests/regression/nods/NOD-1148/testRef3/testRef3.cy.ts
@@ -1,76 +1,46 @@
+import { Ogrs4CalcResult } from 'lib/ogrs'
 import * as oasys from 'oasys'
 
-import { Temporal } from '@js-temporal/polyfill'
 describe('OGRS regression test ref 3', () => {
 
     it('Test ref 3 part 1', () => {
+
+        oasys.login(oasys.Users.probHeadPdu)
 
         // Get offender details
         cy.task('retrieveValue', 'offender').then((offenderData) => {
 
             const offender: OffenderDef = JSON.parse(offenderData as string)
+            // const offender: OffenderDef = {
+            //     forename1: 'Autotest',
+            //     gender: 'Male',
+            //     dateOfBirth: { years: -25 },
+            //     probationCrn: 'ZXCUTIV',
+            //     pnc: '41/7061614Q',
+            // }
 
-            oasys.login(oasys.Users.probHeadPdu)
-            // oasys.Offender.searchAndSelectByPnc(offender.pnc)
-            // oasys.Assessment.createProb({ purposeOfAssessment: 'Risk of Harm Assessment' })
-            // oasys.Populate.fullyPopulated({ layer: 'Layer 1V2' })
-            oasys.Nav.history()
+            oasys.Offender.searchAndSelectByPnc(offender.pnc)
+            oasys.Assessment.createProb({ purposeOfAssessment: 'Risk of Harm Assessment' })
+            oasys.Populate.fullyPopulated({ layer: 'Layer 1V2' })
+            // oasys.Nav.history()            
 
             const predictors = new oasys.Pages.Assessment.RoshaPredictors().goto()
-            predictors.save.click()
-            const ogrsInput: Ogrs4Params = {
-                assessmentDate: {},
-                staticCalc: 'N',
-                dob: { years: -25 },
-                gender: 'Male',
-                offenceCode: '02801',
-                totalSanctionsCount: 5,
-                totalViolentSanctions: 1,
-                contactAdultSanctions: 0,
-                contactChildSanctions: 0,
-                indecentImageSanctions: 0,
-                paraphiliaSanctions: 2,
-                strangerVictim: 'Y',
-                ageAtFirstSanction: 23,
-                lastSanctionDate: { days: -7 },
-                dateRecentSexualOffence: { years: -5 },
-                currSexOffMotivation: 'Y',
-                mostRecentOffence: null,
-                communityDate: { months: 18 },
-                onePointThirty: 'Y',
-                twoPointTwo: 'Y',
-                threePointFour: 1,
-                fourPointTwo: 'Y',
-                sixPointFour: 2,
-                sixPointSeven: 0,
-                sixPointEight: 'In a relationship, living together',
-                sevenPointTwo: 1,
-                dailyDrugUser: 'N',
-                eightPointEight: 0,
-                ninePointOne: 1,
-                ninePointTwo: 0,
-                elevenPointTwo: 0,
-                elevenPointFour: 1,
-                twelvePointOne: 1,
-                aggravatedBurglary: 1,
-                arson: 1,
-                criminalDamageLife: 1,
-                firearms: 1,
-                gbh: 1,
-                homicide: 1,
-                kidnap: 1,
-                robbery: 1,
-                weaponsNotFirearms: 1,
-                custodyInd: 'N',
+            // predictors.o1_39.setValue('Yes')
+            // predictors.save.click()
 
-            }
 
-            const ogrsCalc = oasys.Ogrs.calculateOgrs4(ogrsInput, 'ogrs')
+            const ogrsCalc = oasys.Ogrs.checkOgrs4CalcsOffender(offender, 'ogrs')
             cy.get<Ogrs4CalcResult>('@ogrs').then((ogrs) => {
-                cy.log(JSON.stringify(ogrs))
-                predictors.arp.checkValue(ogrs.arpResult)
+                predictors.arpText.checkValue(ogrs.arpText)
+                predictors.vrpText.checkValue(ogrs.vrpText)
+                predictors.svrpText.checkValue(ogrs.svrpText)
+                predictors.dcSrpBand.checkValue(ogrs.dcSrpBand)
+                predictors.iicSrpBand.checkValue(ogrs.iicSrpBand)
+                predictors.csrpBand.checkValue(ogrs.csrpBand)
+                predictors.csrpType.checkValue(ogrs.csrpType)
+                predictors.csrpScore.checkValue(ogrs.csrpScore)
 
-                // oasys.Assessment.signAndLock({ expectRsrScore: true })
+                oasys.Assessment.signAndLock({ page: oasys.Pages.Rosh.RiskManagementPlan, expectRsrScore: true })
 
                 oasys.logout()
             })
