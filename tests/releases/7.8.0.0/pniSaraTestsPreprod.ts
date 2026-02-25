1516c8f07f5dcf263f846af7e54137e53b1ee8d7
mhiorns-moj <martin.hiorns@justice.gov.uk>
2026-02-25 10:57:14 +0000

    Added pre-prod test for PNI SARA parameters

---
+++ tests/releases/7.8.0.0/pniSaraTestsPreprod.ts
@@ -0,0 +1,58 @@
+import * as oasys from 'oasys'
+
+describe('Pre-prod tests for PNI SARA parameters', () => {
+
+
+    const testCases = [
+        // First set return incorrect values for SARA parameters
+        ['V217229', null],
+        ['E445066', null],
+        ['E475550', null],
+        ['T027274', null],
+        ['E496953', null],
+        ['E050428', null],
+        ['E506857', null],
+        [null, 'A9029EY'],          //  (case raised by John for NOD-1196)
+        ['E599091', null],          //  - expected 2, received 1
+        ['E716844', null],          //  - expected 1, received 2
+        ['E679100', null],          //  - expected 2, received 1
+        ['E591983', null],          //  - 1.30 not reported missing
+        ['S016052', null],          //  - 1.30 not reported missing
+        [null, 'A2144FH'],          //  - 1.30 not reported missing
+    ]
+
+    it(`Pre-prod tests for PNI SARA parameters`, () => {
+        runTest(testCases)
+    })
+
+    function runTest(offenders: string[][]) {
+
+        let failed = false
+        let count = 1
+
+        offenders.forEach((offender) => {
+            cy.task('consoleLog', `Offender ${count++}: ${offender[0]} / ${offender[1]}`)
+
+            if (offender[0] != null) {  // call with probation CRN
+                oasys.Api.testOneOffender(offender[0], 'prob', 'probationFailedAlias', false, true)
+                cy.get<boolean>('@probationFailedAlias').then((offenderFailed) => {
+                    if (offenderFailed) {
+                        cy.task('consoleLog', 'Failed')
+                        failed = true
+                    }
+                })
+            }
+            if (offender[1] != null) {  // call with NomisId
+                oasys.Api.testOneOffender(offender[1], 'pris', 'prisonFailedAlias', offender[0] != null, true)  // skipPrisSubsequents if already done for prob crn
+                cy.get<boolean>('@prisonFailedAlias').then((offenderFailed) => {
+                    if (offenderFailed) {
+                        cy.task('consoleLog', 'Failed')
+                        failed = true
+                    }
+                })
+            }
+        })
+        cy.then(() => { if (failed) throw new Error('Error running RestAPI tests.') })
+    }
+
+})
